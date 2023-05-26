c353ca37825de41616e41ac23f9e15e8bc297882
Alever <759636538@qq.com>
2023-05-26 22:55:54 +0800

    [update]

--- dist_electron/index.js
+++ dist_electron/index.js
@@ -1859,7 +1859,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var elec
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("module.exports = __webpack_require__(/*! C:\\Users\\Alever Lai\\source\\repos\\VFluentForElectron\\src\\background.js */\"./src/background.js\");\n\n\n//# sourceURL=webpack:///multi_./src/background.js?");
+eval("module.exports = __webpack_require__(/*! C:\\Users\\Alever\\source\\repos\\VFluentForElectron\\src\\background.js */\"./src/background.js\");\n\n\n//# sourceURL=webpack:///multi_./src/background.js?");
 
 /***/ }),
 
